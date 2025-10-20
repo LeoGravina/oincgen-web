76a6339b0142155e316c6d1a28cd95eb5f080d94
Leonardo Gravina <leonardocarlos807@gmail.com>
2025-10-20 09:35:19 -0300

    Update Mercado.jsx agora com as frases completas

--- src/components/Sections/Mercado.jsx
+++ src/components/Sections/Mercado.jsx
@@ -8,11 +8,11 @@ function Mercado() {
       <h2>Análise de Mercado</h2>
         <div className="card">
           <h3>Antes da Porteira</h3>
-          <p> A OincGen é influenciada por fatores externos como clima, preço de commodities e políticas públicas...</p>
+          <p>A OincGen é influenciada por fatores externos como clima, preço de commodities e políticas públicas. Condições climáticas extremas afetam a produção de grãos, elevando o custo da ração. Políticas governamentais, legislações ambientais e a demanda global por carne suína impactam diretamente os custos e oportunidades de produção.</p>
           <h3>Dentro da Porteira</h3>
-          <p> Internamente, destacam-se os investimentos em genética, biossegurança e bem estar animal...</p>
+          <p>Internamente, destacam-se os investimentos em genética, biossegurança e bem estar animal. A capacitação da equipe, nutrição adequada e controle sanitário são fundamentais para garantir a produtividade e a saúde dos leitões.</p>
           <h3>Depois da Porteira</h3>
-          <p>Após a produção, fatores logísticos, de comercialização e regulamentação sanitária influenciam a entrega...</p>
+          <p>Após a produção, fatores logísticos, de comercialização e regulamentação sanitária influenciam a entrega dos leitões. O transporte e as certificações de qualidade impactam a competitividade e a imagem da OincGen perante o mercado</p>
         </div>
     </section>
   );
